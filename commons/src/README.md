508f13a84b4286ae4a0a5b8ce1ed7128635a8c12
deep-ink-ventures <chp@deep-ink.ventures>
2023-08-24 15:22:36 +0200

    docs commons

---
+++ commons/src/README.md
@@ -0,0 +1,7 @@
+# Commons Library for MultiClique
+
+[![Stellar](https://img.shields.io/badge/Stellar-v1.0-blue.svg)](https://www.stellar.org)
+[![Rust](https://img.shields.io/badge/Rust-v1.73.0-orange.svg)](https://www.rust-lang.org)
+[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-yellow.svg)](https://opensource.org/licenses/Apache-2.0)
+
+The `commons` library is a part of the MultiClique project, providing common functionality and reusable components for the MultiClique ecosystem. It aims to simplify development, reduce code duplication, and enhance maintainability across various parts of MultiClique.
--- commons/src/traits.rs
+++ commons/src/traits.rs
@@ -1,7 +1,34 @@
 use soroban_sdk::{Address, Env, Symbol, Val, Vec, BytesN};
 
+/// The `MultiCliquePolicyTrait` defines the interface for interacting with the MultiClique policy system.
+/// It provides methods to get the required signing threshold for executing a function and to run the policy
+/// for a given function call. The trait is designed to offer flexibility and adaptability for various policy requirements.
 pub trait MultiCliquePolicyTrait {
+    /// Determines the required threshold of signers for a specific function call.
+    ///
+    /// # Parameters
+    /// * `env`: The environment that provides access to the contract's storage and other functionalities.
+    /// * `num_signers`: The total number of signers in the MultiClique system.
+    /// * `signers`: A vector of signers represented by their public keys.
+    /// * `address`: The address of the contract that the function belongs to.
+    /// * `fn_name`: The symbol representing the function name.
+    /// * `args`: A vector of values representing the arguments for the function call.
+    ///
+    /// # Returns
+    /// Returns the required number of signers (threshold) to execute the specified function.
     fn get_threshold(env: Env, num_signers: u32, signers: Vec<BytesN<32>>, address: Address, fn_name: Symbol, args: Vec<Val>) -> u32;
 
+    /// Executes the policy logic for a given function call.
+    ///
+    /// This method runs the policy for a specific function, taking into account the signers, address, function name, and arguments.
+    /// It may include additional checks, validations, or operations based on the policy implementation.
+    ///
+    /// # Parameters
+    /// * `env`: The environment that provides access to the contract's storage and other functionalities.
+    /// * `num_signers`: The total number of signers in the MultiClique system.
+    /// * `signers`: A vector of signers represented by their public keys.
+    /// * `address`: The address of the contract that the function belongs to.
+    /// * `fn_name`: The symbol representing the function name.
+    /// * `args`: A vector of values representing the arguments for the function call.
     fn run_policy(env: Env, num_signers: u32, signers: Vec<BytesN<32>>, address: Address, fn_name: Symbol, args: Vec<Val>);
 }
